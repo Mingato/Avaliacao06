e39e6c007cbac304a1356cf0dcea7b4a86534a3f
Gunter <gunter@redcompany.com.br>
2020-12-21 19:42:39 -0300

    First commit

---
+++ src/main/java/com/mc646/avaliacao06/Main.java
@@ -0,0 +1,10 @@
+package com.mc646.avaliacao06;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Ola");
+	}
+
+}
---
+++ src/test/java/com/mc646/avaliacao06/Avaliacao06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mc646.avaliacao06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Avaliacao06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
